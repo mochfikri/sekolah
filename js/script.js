38450f1c1e8e9c8de4aeae9df3535812a52c487e
Moch Fikri <mochfikri050201@gmail.com>
2018-04-15 18:45:29 +0700

    parallax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-const burgerNav   = document.getElementById('burger-nav');
-const menuMobile  = document.getElementById("menu-mobile");
-burgerNav.onclick = function(){
-  (this).classList.toggle("change");
-  menuMobile.classList.toggle("menu-muncul")
-}
+$(document).ready(function() {
+
+  $('#burger-nav').on('click',function(){
+    $(this).toggleClass("change");
+    $("#menu-mobile").toggleClass("menu-muncul");
+  });
+
+  $(window).scroll(function() {
+
+    const wScroll = $(this).scrollTop();
+
+    showAnimate(".intro-box", "intro-show", 700);
+    showAnimate(".service-box", "service-show", 700);
+    showAnimate(".news-box", "news-show", 700);
+    showAnimate(".alumni-box", "alumni-show", 700);
+    showAnimate(".teacher-box", "teacher-show", 700);
+
+    function showAnimate(target, name, offset)
+    {
+      $(target).each(function(i){
+        if (wScroll > $(target).eq(i).offset().top - offset) {
+            setTimeout(function(){
+               $(target).eq(i).addClass(name);
+            },300 * (i+1));
+        };
+      });
+    }
+
+  }); //end scroll
+
+}); //end ready
